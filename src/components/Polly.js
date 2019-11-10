3b79855d92137446c172bcca3df477d5acf543b1
Nicholas Walsh <nicholaswalsh.official@gmail.com>
2019-11-10 03:01:13 -0800

    remove unnecessary initial voice state

--- src/components/Polly.js
+++ src/components/Polly.js
@@ -11,7 +11,7 @@ class Polly extends Component {
       
         this.state = {
           text: '',
-          voiceId: 'ivy',
+          voiceId: '',
           engine: '',
           resultMessage: '',
           pollyUrl: ''
