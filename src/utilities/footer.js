6978fa142b234199fb972f343bd137667a6e6fdb
Nicholas Walsh <nicholaswalsh.official@gmail.com>
2020-03-16 15:21:27 -0700

    fix to actually remove aws footer

--- src/utilities/footer.js
+++ src/utilities/footer.js
@@ -6,7 +6,7 @@ export default class Footer extends Component {
         <div className="row">
             <div className="footer-demo text-center">
                 <style>position:relative;</style>
-                <p>{/*&copy; {new Date().getFullYear()} Amazon Web Services, Inc. or its affiliates. All rights reserved. <br></br> */}Made with ♥ by <a href="https://twitter.com/thenickwalsh" target="_blank" rel="noopener noreferrer">Nick Walsh</a> and <a href="https://twitter.com/kneekey23" target="_blank" rel="noopener noreferrer">Nicki Stone</a></p>
+                <p>Made with ♥ by <a href="https://twitter.com/thenickwalsh" target="_blank" rel="noopener noreferrer">Nick Walsh</a> and <a href="https://twitter.com/kneekey23" target="_blank" rel="noopener noreferrer">Nicki Stone</a></p>
             </div>
         </div>
     )
